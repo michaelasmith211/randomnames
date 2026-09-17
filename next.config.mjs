782e8b344dbfe14e2240e5e5a908421d280112be
michaelasmith211 <michaelasmith211@users.noreply.github.com>
2026-09-17 15:04:28 +0530

    fix(styles): set basePath to root for custom domain randomnamegenerator.dev and add CNAME

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-// If deployed to <username>.github.io/<repo>, basePath is /<repo>
-// If a custom domain is used, basePath should be empty
-const repoName = 'randomnames';
-const basePath = isGithubActions && !process.env.CUSTOM_DOMAIN ? `/${repoName}` : '';
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: basePath,
+  basePath: '',
   images: {
     unoptimized: true,
   },
