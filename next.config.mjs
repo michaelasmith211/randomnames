302f4aa01bfa619a9973aeeb30abdfacca7b67cf
michaelasmith211 <michaelasmith211@users.noreply.github.com>
2026-09-17 14:51:59 +0530

    fix(styles): fix PostCSS Tailwind CSS compilation and configure automated GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// If deployed to <username>.github.io/<repo>, basePath is /<repo>
+// If a custom domain is used, basePath should be empty
+const repoName = 'randomnames';
+const basePath = isGithubActions && !process.env.CUSTOM_DOMAIN ? `/${repoName}` : '';
+
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
   trailingSlash: true,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
   poweredByHeader: false,
 };
 
